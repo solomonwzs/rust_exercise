06162ce24a51b56d519caee9eb986b3df7656d9b
solomon <solomon.wzs@gmail.com>
2018-08-30 20:09:11 +0800

    modified:   src/main.rs

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
@@ -10,7 +17,7 @@ fn first_word(s: &str) -> &str {
     &s[..]
 }
 
-fn main() {
+fn test_first_word() {
     let s = String::from("hello world");
     let word = first_word(&s);
 
@@ -21,4 +28,19 @@ fn main() {
 
     // let s = &s[20..];
     // println!("[{}]", s);
+}
+
+fn test_struct() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
+}
+
+fn main() {
+    test_first_word();
+    test_struct();
 }
