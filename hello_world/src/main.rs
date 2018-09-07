17b885f1fbd6c52af29e7553945a7acb021e77d8
solomon <solomon.wzs@gmail.com>
2018-09-07 18:45:04 +0800

    modified:   hello_world/src/main.rs 	new file:   minigrep/src/lib.rs 	modified:   minigrep/src/main.rs

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -7,9 +7,18 @@ pub mod t_string;
 pub mod t_struct;
 pub mod t_vectors;
 
+fn foo(s: &mut String) {
+    s.push_str("b")
+}
+
 fn main() {
     println!("hello world!");
 
-    let v = vec![1, 2, 3];
-    v[99];
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    let mut s = String::from("a");
+    let s0 = &mut s;
+    foo(s0);
+    println!("{}", s0);
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,19 @@
+extern crate minigrep;
+
 use std::env;
+use std::process;
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let conf = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("error: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("{} {}", query, filename);
+    if let Err(err) = minigrep::run(conf) {
+        eprintln!("error: {}", err);
+        process::exit(1);
+    }
 }
