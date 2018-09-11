e45440aeb448a96d7ab04513197a277d881aea8d
solomon <solomon.wzs@gmail.com>
2018-09-11 18:57:17 +0800

    modified:   src/t_pointers.rs

--- hello_world/src/t_pointers.rs
+++ hello_world/src/t_pointers.rs
@@ -1,8 +1,9 @@
 use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Result;
+use std::ops::Deref;
 
-pub enum List {
+enum List {
     Cons(i32, Box<List>),
     Nil,
 }
@@ -16,8 +17,17 @@ impl Display for List {
     }
 }
 
+struct MyBox<T> (T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
     use super::List::{Cons, Nil};
 
     #[test]
@@ -26,5 +36,15 @@ mod test {
         let list = Cons(1, Box::new(Cons(2, Box::new(
                         Cons(3, Box::new(Nil))))));
         println!("{}", list);
+    }
+
+    #[test]
+    fn it_work0() {
+        let x = 5;
+        // let y = MyBox::new(x);
+        let y = Box::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
     }
 }
