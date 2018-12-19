eb00137c033546ac390c8d5fedbce76356fb1839
solomon <solomon.wzs@gmail.com>
2018-12-19 19:25:54 +0800

    modified:   ../Cargo.toml 	new file:   Cargo.toml 	new file:   src/lib.rs 	new file:   src/main.rs

---
+++ mock_test/src/lib.rs
@@ -0,0 +1,67 @@
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where T: Messenger {
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let per_of_max = self.value as f64 / self.max as f64;
+        if per_of_max >= 0.75 && per_of_max < 0.9 {
+            self.messenger.send("warning");
+        } else if per_of_max >= 0.9 && per_of_max < 1.0 {
+            self.messenger.send("urgent");
+        } else if per_of_max >= 1.0 {
+            self.messenger.send("error");
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger{ sent_messages: RefCell::new(vec![]) }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            // self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
